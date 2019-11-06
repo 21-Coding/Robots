6cbc422d5082ebe30642ddbd47e4ac6c80a88c83
Student <example@example.com>
2019-11-06 15:49:27 -0800

    add rock em repo

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+// a robot = {
+//   hitPoints: 10,
+//   sock: 5
+// }
+
+function Game(score, robot) {
+  this.score = score;
+  this.robot = robot;
+  // this.robot = robot;
+}
+
+// {
+//   score: 1,
+//   robot: {
+//     hitPoints: 10,
+//     sock: 5
+//   }
+// }
+
+var sock = 0;
+var test = game.sockEm();
+Game.prototype.sockEm = function() {
+
+  this.robot.sock += 1
+
+ return sock +=1;
+  };
+
+
+// function modifiedScore(score, newScore) {
+//   return sock();
+// }
+//
+// var rockEm = new Game ;
+//   console.log(modifiedScore());
+//
+//
+//
+//
+//
+// var score "";
+// ().click(function(){
+//   score += 1
+// })
