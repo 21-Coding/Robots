fc9f16b6a21d6c6bc491002927757b2d969194dd
Student <example@example.com>
2019-11-07 11:56:20 -0800

    make score increase by 1 on button click and display on screen. not in scripts2.

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,39 @@ function Game (){
   this.score = 0
 }
 
-//add 1 to score ---- = A ... increase score a..
-
 //robot in funct
 Game.prototype.addScore = function() {
     this.score += 1
     return this.score
   };
+
+
+Game.prototype.switchUser = function() {
+  math.floor(math.random(20))
+  if (this.score < 10){
+    this.addScore
+  }else {
+    false
+  };
+}
+
+$(document).ready(function() {
+  var newGame = new Game();
+  // var newScore = []
+  $("#robotbutton").click(function(event) {
+    event.preventDefault();
+    newGame.addScore();
+
+    console.log(newGame.score);
+    $("#damage").text(newGame.score);
+  });
+
+})
+
+
+
+
+
 
 
 // math.random(50)
@@ -18,70 +44,3 @@ Game.prototype.addScore = function() {
 // 2. add function that is random
 // 3. click function
 // 4. branching to determine score
-
-Game.prototype.switchUser = function() {
-  // if users random number is more than 10 switch turn
-}
-
-
-
-
-//Ui
-
-
-  // function Robot() {
-  //   this.hit =
-  //
-  //
-  // }
-$(document).ready(function() {
-  var newGame = new Game();
-
-  $("#robotbutton").click(function(event) {
-    event.preventDefault();
-    newGame.addScore();
-    console.log(newGame.score);
-    return newGame.score
-  });
-
-
-})
-
-
-//
-// click button >>> addScore('A')
-// click button >>> addScore('B')
-
-
-
-// (this.score).forEach(function(){
-//   return this.score +=1,
-// });
-//   return sockEm();
-// }
-// //
-// var robot = new Robot;
-//   console.log(modifiedScore());
-//// function modifiedScore(hitPoints, newPoints) {
-//
-//
-//
-//
-// var score "";
-// ().click(function(){
-//   score += 1
-// })
-// a robot = {
-//   hitPoints: 10,
-//   sock: 5
-// }
-// {
-//   score: 1,
-//   robot: {
-//     hitPoints: 10,
-//     sock: 5
-//   }
-// }
-// var sockEm = (hitPoints)
-// this.sock = sock;
-// this.robot = robot;
