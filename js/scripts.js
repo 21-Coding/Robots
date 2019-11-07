1165ed9b5a8bd213708be8a4badc4dd63f65cee9
Student <example@example.com>
2019-11-07 11:31:25 -0800

    add js that returns increments by one

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,68 @@
-// a robot = {
-//   hitPoints: 10,
-//   sock: 5
-// }
+function Game (){
 
-function Game(score, robot) {
-  this.score = score;
-  this.robot = robot;
-  // this.robot = robot;
+  this.score = 0
 }
 
-// {
-//   score: 1,
-//   robot: {
-//     hitPoints: 10,
-//     sock: 5
-//   }
-// }
+//add 1 to score ---- = A ... increase score a..
 
-var sock = 0;
-var test = game.sockEm();
-Game.prototype.sockEm = function() {
-
-  this.robot.sock += 1
-
- return sock +=1;
+//robot in funct
+Game.prototype.addScore = function() {
+    this.score += 1
+    return this.score
   };
 
 
-// function modifiedScore(score, newScore) {
-//   return sock();
+// math.random(50)
+// user will click a button that adds score by random number, computer will also function off of similar code. Will switch() users randomly inbetween turns similar to dice roll.
+// 1. switch function that is random
+// 2. add function that is random
+// 3. click function
+// 4. branching to determine score
+
+Game.prototype.switchUser = function() {
+  // if users random number is more than 10 switch turn
+}
+
+
+
+
+//Ui
+
+
+  // function Robot() {
+  //   this.hit =
+  //
+  //
+  // }
+$(document).ready(function() {
+  var newGame = new Game();
+
+  $("#robotbutton").click(function(event) {
+    event.preventDefault();
+    newGame.addScore();
+    console.log(newGame.score);
+    return newGame.score
+  });
+
+
+})
+
+
+//
+// click button >>> addScore('A')
+// click button >>> addScore('B')
+
+
+
+// (this.score).forEach(function(){
+//   return this.score +=1,
+// });
+//   return sockEm();
 // }
-//
-// var rockEm = new Game ;
+// //
+// var robot = new Robot;
 //   console.log(modifiedScore());
-//
+//// function modifiedScore(hitPoints, newPoints) {
 //
 //
 //
@@ -42,3 +71,17 @@ Game.prototype.sockEm = function() {
 // ().click(function(){
 //   score += 1
 // })
+// a robot = {
+//   hitPoints: 10,
+//   sock: 5
+// }
+// {
+//   score: 1,
+//   robot: {
+//     hitPoints: 10,
+//     sock: 5
+//   }
+// }
+// var sockEm = (hitPoints)
+// this.sock = sock;
+// this.robot = robot;
---
+++ js/scripts2.js
@@ -0,0 +1,39 @@
+// a robot = {
+//   hitPoints: 10,
+//   sock: 5
+// }
+
+function Robot(hitPoints) {
+  this.score = 0
+  // this.sock = sock;
+  // this.robot = robot;
+}
+
+// {
+//   score: 1,
+//   robot: {
+//     hitPoints: 10,
+//     sock: 5
+//   }
+// }
+// var sockEm = (hitPoints)
+Robot.prototype.score = function() {
+   return newScore += 1;
+  };
+ var newGame = new Robot;
+// function modifiedScore(hitPoints, newPoints) {
+console.log(newGame.sockEm(1))
+//   return sockEm();
+// }
+// //
+// var robot = new Robot;
+//   console.log(modifiedScore());
+//
+//
+//
+//
+//
+// var score "";
+// ().click(function(){
+//   score += 1
+// })
